6d76b5312f06484cb7bf4dbb2a5db5e2d32925c4
rubywai <rubyland.myanmar.mg@gmail.com>
2022-04-11 00:50:20 +0630

    recyclerview delete callback

--- app/src/main/java/com/rubylearner/scoped/noteapplication/feature/note/presentation/view/adapter/NoteListAdapter.kt
+++ app/src/main/java/com/rubylearner/scoped/noteapplication/feature/note/presentation/view/adapter/NoteListAdapter.kt
@@ -3,19 +3,21 @@ package com.rubylearner.scoped.noteapplication.feature.note.presentation.view.ad
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.rubylearner.scoped.cleanarchitecturetodolist.R
 import com.rubylearner.scoped.cleanarchitecturetodolist.databinding.NoteLayoutBinding
 import com.rubylearner.scoped.noteapplication.feature.note.domain.entity.NoteEntity
 
-class NoteListAdapter(private val notes : List<NoteEntity>) :  RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>(){
+class NoteListAdapter(private val notes : List<NoteEntity>,private val onDelete : OnDelete) :  RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>(){
     class NoteViewHolder(view : NoteLayoutBinding) : RecyclerView.ViewHolder(view.root){
         var textView : TextView = view.txtNote
+        var delete : Button = view.delete
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val view = com.rubylearner.scoped.cleanarchitecturetodolist.databinding.NoteLayoutBinding.inflate(
+        val view = NoteLayoutBinding.inflate(
             LayoutInflater.from(parent.context),parent,false
         )
         return NoteViewHolder(view)
@@ -23,7 +25,14 @@ class NoteListAdapter(private val notes : List<NoteEntity>) :  RecyclerView.Adap
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         holder.textView.text = notes.get(position).note
+        holder.delete.setOnClickListener {
+            onDelete.onDelete(position)
+        }
     }
 
     override fun getItemCount(): Int = notes.size
+
+    interface OnDelete{
+        fun onDelete(position: Int)
+    }
 }
--- app/src/main/java/com/rubylearner/scoped/noteapplication/feature/note/presentation/view/fragment/NoteListFragment.kt
+++ app/src/main/java/com/rubylearner/scoped/noteapplication/feature/note/presentation/view/fragment/NoteListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -14,7 +15,7 @@ import com.rubylearner.scoped.cleanarchitecturetodolist.databinding.NoteListScre
 import com.rubylearner.scoped.noteapplication.feature.note.presentation.view.adapter.NoteListAdapter
 import com.rubylearner.scoped.noteapplication.feature.note.presentation.viewmodel.NoteViewModel
 
-class NoteListFragment : Fragment(R.layout.note_list_screen) {
+class NoteListFragment : Fragment(R.layout.note_list_screen),NoteListAdapter.OnDelete {
     private  var noteListScreenBinding : NoteListScreenBinding? = null
     private  val binding get() = noteListScreenBinding!!
     private  val noteViewModel : NoteViewModel by activityViewModels()
@@ -27,7 +28,7 @@ class NoteListFragment : Fragment(R.layout.note_list_screen) {
         noteListScreenBinding = NoteListScreenBinding.inflate(layoutInflater,container,false)
         lifecycleScope.launchWhenStarted {
             noteViewModel.noteState.collect{
-                noteListAdapter = NoteListAdapter(it)
+                noteListAdapter = NoteListAdapter(it,this@NoteListFragment)
                 binding.noteListRecycler.layoutManager = LinearLayoutManager(context)
                 binding.noteListRecycler.adapter = noteListAdapter
             }
@@ -41,5 +42,9 @@ class NoteListFragment : Fragment(R.layout.note_list_screen) {
     override fun onDestroy() {
         noteListScreenBinding = null
         super.onDestroy()
+    }
+
+    override fun onDelete(position: Int) {
+        Toast.makeText(context, "$position", Toast.LENGTH_SHORT).show()
     }
 }
