3b65f3c45285b53f9c839e91c34def14d7e547b1
rubywai <rubyland.myanmar.mg@gmail.com>
2022-04-12 23:20:07 +0630

    editing

---
+++ app/src/main/java/com/rubylearner/scoped/noteapplication/feature/note/presentation/view/fragment/NoteEditFragment.kt
@@ -0,0 +1,7 @@
+package com.rubylearner.scoped.noteapplication.feature.note.presentation.view.fragment
+
+import androidx.fragment.app.Fragment
+import com.rubylearner.scoped.cleanarchitecturetodolist.R
+
+class NoteEditFragment : Fragment(R.layout.fragment_note_edit) {
+}
